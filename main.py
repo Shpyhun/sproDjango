9b5fdc67d20177d6edba5fc0fbcc7de931cdfda1
Volodymyr Shpyhun <vladimir.shpigun@gmail>
2022-06-05 22:17:26 +0300

    First project by django

---
+++ main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+Ваши действия:
+
+Прочитать модель OSI.
+Изучить статус коды.
+Написать уровни OSI.
+Установить и запустить Django
+
+Задание 1
+
+В созданном проекте создайте приложение firstapp;
+добавьте в него файл urls.py;
+создайте urlpattern;
+подключите к этому url адресу функцию обработчик hellodjango;
+реализуйте функцию hellodjango, чтобы она с помощью метода HttpResponse по пустому url выводила в браузер фразу "Hello Django!".
+
+Задание 2
+
+Создайте url path("<str:name>/"), и функцию обработчик которая будет выводить в браузер f"Hello {name}!"
+
+
+Задание 3
+
+Создайте обработчики, чтобы:
+
+по url “date/” выводил дату (22.03.2022);
+по url “date/year/” выводил 2022, по url “date/day/” выводил 22;
+по url “date/month/” выводил 03.
+"""
